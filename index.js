2f23d3fbdbad6c671f3eea4a27396fe2f2a523ef
IsaacOrtga <ortga.isaac@gmail.com>
2024-07-02 22:15:48 +0200

    navbar finished mobilefirst

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    let line_1 = document.getElementById('line_1');
+    let line_2 = document.getElementById('line_2');
+    let line_3 = document.getElementById('line_3');
+
+    // line_3.style.display = "none";
+    line_1.classList.add("rotate-animation");
+})
