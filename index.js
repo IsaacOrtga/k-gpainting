e5e8ed74d0775f307e4b52b159e09cbb8a7c6b33
IsaacOrtga <ortga.isaac@gmail.com>
2024-07-04 23:58:31 +0200

    upload js effects

--- index.js
+++ index.js
@@ -17,4 +17,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     const observer = new IntersectionObserver(observerCallback, observerOptions);
     observer.observe(serviceSection);
+
+
+    // ****** Contact-Button Observer ******//
+    const serviceContact = document.querySelector('.contact-button');
+    const observerContactOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observerContactCallback = (entries, observer) => {
+        entries.forEach(entry  => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    };
+
+    const observerContact = new IntersectionObserver(observerContactCallback, observerContactOptions);
+    observerContact.observe(serviceContact);
+
 })
