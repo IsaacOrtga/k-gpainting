b8ffd8f43ed7fe45d6fbbfbaa8e05664e5d426f6
IsaacOrtga <ortga.isaac@gmail.com>
2024-07-05 02:09:47 +0200

    upload review js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const serviceSection = document.querySelector('.service-section');
+    const excellenceSection = document.querySelector('.container-excellence');
+    const serviceContact = document.querySelector('.contact-button');
+
     const observerOptions = {
         root: null,
         rootMargin: '0px',
@@ -17,26 +20,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     const observer = new IntersectionObserver(observerCallback, observerOptions);
     observer.observe(serviceSection);
+    observer.observe(excellenceSection);
+    observer.observe(serviceContact);
 
 
-    // ****** Contact-Button Observer ******//
-    const serviceContact = document.querySelector('.contact-button');
-    const observerContactOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1
-    };
 
-    const observerContactCallback = (entries, observer) => {
-        entries.forEach(entry  => {
-            if(entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    };
+    const myCarousel = document.querySelector('#carouselExampleControls');
+    const carousel = new bootstrap.Carousel(myCarousel, {
+        interval: 3000, // Intervalo de desplazamiento automático en milisegundos (ej. 5000 = 5 segundos)
+        wrap: true // Permite que el carrusel vuelva al principio después de la última diapositiva
+    });
 
-    const observerContact = new IntersectionObserver(observerContactCallback, observerContactOptions);
-    observerContact.observe(serviceContact);
+    // Muestra las flechas al pasar el mouse sobre el carrusel
+    myCarousel.addEventListener('mouseenter', function() {
+        myCarousel.querySelector('.carousel-control-prev').style.visibility = 'visible';
+        myCarousel.querySelector('.carousel-control-next').style.visibility = 'visible';
+    });
 
+    // Oculta las flechas cuando el mouse sale del carrusel
+    myCarousel.addEventListener('mouseleave', function() {
+        myCarousel.querySelector('.carousel-control-prev').style.visibility = 'hidden';
+        myCarousel.querySelector('.carousel-control-next').style.visibility = 'hidden';
+    });
 })
